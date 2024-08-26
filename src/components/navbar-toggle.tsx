1a5d2ae11fd7b34f92de617cfa4f8fd4c59afeda
immobe <i.beeck@gmail.com>
2024-08-27 00:12:18 +0200

    refactor: Improve accessibility and interactivity of NavbarToggle component

--- src/components/navbar-toggle.tsx
+++ src/components/navbar-toggle.tsx
@@ -2,7 +2,19 @@ import type {FunctionalComponent} from 'preact';
 import React from 'preact/compat';
 
 const NavbarToggle: FunctionalComponent = () => {
-  return <div></div>;
+  return (
+    <button
+      className="group relative h-[18px] w-[30px]"
+      onClick={e => {
+        e.currentTarget.classList.toggle('open');
+      }}>
+      <span className="nav-icon-span top-0 group-[.open]:left-1/2 group-[.open]:top-[9px] group-[.open]:w-0"></span>
+      <span className="nav-icon-span top-[9px] group-[.open]:rotate-45"></span>
+      <span className="nav-icon-span top-[9px] group-[.open]:rotate-[-45deg]"></span>
+      <span className="nav-icon-span top-[18px] group-[.open]:left-1/2 group-[.open]:top-[9px] group-[.open]:w-0"></span>
+      ;
+    </button>
+  );
 };
 
 export default NavbarToggle;
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,22 +1,13 @@
 // Make sure to import the CSS file
 import type {FunctionalComponent} from 'preact';
 import React from 'preact/compat';
+import NavbarToggle from './navbar-toggle';
 
 const Navbar: FunctionalComponent = () => {
   return (
     <nav className="">
       <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4">
-        <button
-          className="group relative h-[18px] w-[30px]"
-          onClick={e => {
-            e.currentTarget.classList.toggle('open');
-            console.log(e, 'clicked');
-          }}>
-          <span className="nav-icon-span top-0 group-[.open]:left-1/2 group-[.open]:top-[9px] group-[.open]:w-0"></span>
-          <span className="nav-icon-span top-[9px] group-[.open]:rotate-45"></span>
-          <span className="nav-icon-span top-[9px] group-[.open]:rotate-[-45deg]"></span>
-          <span className="nav-icon-span top-[18px] group-[.open]:left-1/2 group-[.open]:top-[9px] group-[.open]:w-0"></span>
-        </button>
+        <NavbarToggle />
         <div className="hidden w-full md:block md:w-auto" id="navbar-dropdown">
           <ul></ul>
         </div>
