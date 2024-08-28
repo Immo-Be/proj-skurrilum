f85e4e4d466cefeef13a2ed64dfe099524e5c8b5
immobe <i.beeck@gmail.com>
2024-08-28 10:22:01 +0200

    refactor: Update tailwind.config.js to add primary and text colors with alpha values

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,16 @@ export default {
       fontFamily: {
         sans: ['Nunito-Regular', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        primary: 'rgb(var(--color-primary) / <alpha-value>)',
+        text: 'rgb(var(--color-text) / <alpha-value>)',
+        // success: 'rgb(var(--color-success) / <alpha-value>)',
+        // info: 'rgb(var(--color-info) / <alpha-value>)',
+        // warn: 'rgb(var(--color-warn) / <alpha-value>)',
+        // error: 'rgb(var(--color-error) / <alpha-value>)',
+        // transparent: 'transparent',
+        // current: 'currentColor',
+      },
     },
   },
   plugins: [],
