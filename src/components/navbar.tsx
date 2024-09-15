777cf504f6c5920094b9e42104ce4c57b2602992
immobe <i.beeck@gmail.com>
2024-09-15 18:17:30 +0200

    feat: Refactor Navbar component to use useIsScrolled hook and improve styling with tailwind-merge

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,28 +1,41 @@
-// Make sure to import the CSS file
 import type {FunctionalComponent} from 'preact';
 import {useState} from 'preact/hooks';
-import Logo from '../assets/logo.png';
-// import {useIsMobile} from '../hooks/use-is-mobile';
+import {twMerge} from 'tailwind-merge';
+import {useIsScrolled} from '../hooks/use-is-scrolled';
 import NavbarListItem from './navbar-list-item';
 import NavbarToggle from './navbar-toggle';
 
 const Navbar: FunctionalComponent = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
-  // const isMobile = useIsMobile();
+
+  const hasScrolled = useIsScrolled(100);
+
   return (
     <>
-      <div className="absolute z-20 flex w-full items-center justify-between bg-transparent p-4">
-        <img src={Logo.src} class={`h-8 ${isOpen && 'hidden'}`} />
+      <header
+        className={twMerge(
+          'fixed z-30 flex w-full items-center justify-between p-2 transition-all duration-100',
+          hasScrolled ? 'bg-primary shadow-md' : 'bg-transparent',
+          isOpen && 'bg-primary shadow-none'
+        )}>
+        <h1 className="before:logo-svg relative h-[32px] w-[64px] before:absolute before:inset-0 before:isolate">
+          <span className="hidden">
+            Skurrilum - Escape Room Hamburg Reeperbahn
+          </span>
+          <a href="/" aria-label="Skurrilum Home"></a>
+        </h1>
         <NavbarToggle
+          customClass="mix-blend-difference"
           onClick={() => setIsOpen(prev => !prev)}
           isOpen={isOpen}
         />
-      </div>
+      </header>
       <nav
-        className={`pointer-events-none fixed bottom-0 left-0 right-0 top-0 z-10 bg-primary text-center opacity-0 transition-opacity duration-500 ease-in ${
-          isOpen ? 'opacity-[0.98]' : ''
-        }`}>
-        <ul className="pointer-events-auto flex h-full items-center justify-center md:flex-row md:flex-nowrap">
+        className={twMerge(
+          'animate-display-none is-deleting pointer-events-none fixed bottom-0 left-0 right-0 top-0 z-10 hidden bg-primary text-center opacity-0 transition-opacity duration-200 ease-in',
+          isOpen ? 'pointer-events-auto block opacity-100' : ''
+        )}>
+        <ul className="pointer-events-auto flex h-full flex-col items-center justify-center md:flex-row md:flex-nowrap">
           <NavbarListItem href="/" text="Home" />
           <NavbarListItem href="/" text="Unsere Räume" />
           <NavbarListItem href="/" text="Gruppen & Firmen" />
