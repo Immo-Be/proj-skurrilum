1fd67f570d395fe98a8b4187491d599ecc285996
immobe <i.beeck@gmail.com>
2024-10-13 15:06:57 +0200

    feat: Enhance internationalization support by integrating room translations into UI

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,16 +1,40 @@
+import room1 from './rooms/der-zoo-des-todes.json';
+
 const defaultLang = 'de' as const;
 
-export const ui = {
+// type En = Record<string, string>;
+
+// type Translation<Enn extends En> = {
+//   en: Enn;
+//   de: {
+//     [Properties in keyof Enn]: string;
+//   };
+// };
+
+// const room1: Translation<En> = {
+
+// } as const;
+
+export const tsdf = {
   en: {
     'nav.home': 'Home',
     'nav.about': 'About',
     'nav.twitter': 'Twitter',
-    title: 'Welcome to the Next.js i18n example',
   },
   de: {
     'nav.home': 'Accueil',
     'nav.about': 'À propos',
-    title: "Bienvenue dans l'exemple de i18n de Next.js",
+  },
+} as const;
+
+export const ui = {
+  en: {
+    ...room1.en,
+    ...tsdf.en,
+  },
+  de: {
+    ...room1.de,
+    ...tsdf.de,
   },
 } as const;
 
