60e3876373e7793eee48d3c823985ce38684fed1
immobe <i.beeck@gmail.com>
2024-12-06 16:54:51 +0100

    feat: remove i18n dependencies and commented code for cleaner configuration

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,6 @@
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-import {i18n, filterSitemapByDefaultLocale} from 'astro-i18n-aut/integration';
+// import {i18n, filterSitemapByDefaultLocale} from 'astro-i18n-aut/integration';
 import {defineConfig} from 'astro/config';
 
 // Todo: Add sitemap filter to i18n integration
@@ -19,17 +19,17 @@ export default defineConfig({
     format: 'directory',
   },
   integrations: [
-    i18n({
-      defaultLocale, // the default locale
-      locales, // the locales you want to support
-    }),
-    sitemap({
-      i18n: {
-        locales,
-        defaultLocale,
-      },
-      filter: filterSitemapByDefaultLocale({defaultLocale}),
-    }),
+    // i18n({
+    //   defaultLocale, // the default locale
+    //   locales, // the locales you want to support
+    // }),
+    // sitemap({
+    //   i18n: {
+    //     locales,
+    //     defaultLocale,
+    //   },
+    //   filter: filterSitemapByDefaultLocale({defaultLocale}),
+    // }),
     tailwind(),
   ],
 });
