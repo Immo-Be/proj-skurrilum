9291516776dcbe846329089a431edf38bd18baf7
immobe <i.beeck@gmail.com>
2025-07-11 15:11:35 +0200

    feat(app): add website files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
   const menuToggle = document.querySelector('.menu-toggle');
   const mainMenu = document.querySelector('#main-menu');
+  const siteHeader = document.querySelector('header');
   const dropdowns = document.querySelectorAll('.has-dropdown > a');
 
   if (menuToggle && mainMenu) {
@@ -9,6 +10,8 @@ document.addEventListener('DOMContentLoaded', function () {
       this.setAttribute('aria-expanded', !isExpanded);
       this.classList.toggle('is-active'); // Toggle active class on the button
       mainMenu.classList.toggle('is-open');
+      console.log('Menu toggle clicked', siteHeader); // Log for debugging
+      siteHeader.classList.toggle('is-open'); // Toggle open class on the header
       document.body.classList.toggle('no-scroll'); // Prevent scrolling when menu is open
     });
   }
