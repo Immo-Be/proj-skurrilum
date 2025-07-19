8fd31bc968577e25cdde8ce86cce668a5e173b27
immobe <i.beeck@gmail.com>
2025-07-19 17:18:21 +0200

    feat(vidoe): add video js file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-  // Video logic
-  const heroVideo = document.getElementById('hero-video');
-
-  if (heroVideo) {
-    const videos = {
-      small: 'webtrailer_lowq.webm',
-      medium: 'webtrailer_midq.webm',
-      large: 'webtrailer.webm',
-    };
-
-    function setVideoSource() {
-      const screenWidth = window.innerWidth;
-      let videoSource;
-
-      if (screenWidth < 768) {
-        videoSource = videos.small;
-      } else if (screenWidth < 1200) {
-        videoSource = videos.medium;
-      } else {
-        videoSource = videos.large;
-      }
-
-      heroVideo.src = `/videos/${videoSource}`;
-    }
-
-    setVideoSource();
-    window.addEventListener('resize', setVideoSource);
-  }
-
   // Original menu logic
   const menuToggle = document.querySelector('.menu-toggle');
   const mainNav = document.querySelector('.main-nav');
